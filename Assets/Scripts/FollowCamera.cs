d53955375e2277e13bb0ea69eb3c1a1ad5994b31
Anton Volkov <xzxz2020.ru@mail.ru>
2021-04-20 14:28:46 +0500

    Обнолвение 3

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    public GameObject Player;
+    private Vector3 offset;  
+
+    void Start () 
+    {        
+        offset = transform.position - Player.transform.position;
+    }
+
+    void LateUpdate () 
+    {        
+        transform.position = Player.transform.position + offset;
+    }
+}
