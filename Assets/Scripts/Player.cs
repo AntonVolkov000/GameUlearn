78f400e7719bdc7e6ed3d939d4a36f53f1245e93
Anton Volkov <xzxz2020.ru@mail.ru>
2021-05-11 18:29:09 +0500

    Перенос в основной Котроллер

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,25 +5,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int currentHealth;
-    public HealthBar healthBar;
+    
 
     void Start()
     {
-        currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
+        
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            TakeDamage(20);  
+        
     }
 
-    void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        healthBar.SetHealth(currentHealth);
-    }
+    
 }
