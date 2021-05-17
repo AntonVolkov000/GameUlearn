047b73cc55d3bafc6d2251d6479926d5a5edbcb7
Anton Volkov <xzxz2020.ru@mail.ru>
2021-05-17 13:11:04 +0500

    Доработка персонажа

--- Assets/Scripts/MageMagicSpell.cs
+++ Assets/Scripts/MageMagicSpell.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MagicSpell : MonoBehaviour
+public class MageMagicSpell : MonoBehaviour
 {
     public float speed;
     public PlayerController player;
